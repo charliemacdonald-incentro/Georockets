2ab59cbbe3c67b52c05fbbfb8661aaa8f4b42a15
charliemacdonald_incentro <charlie.macdonald@incentro.com>
2018-05-09 10:03:48 +0200

    google maps implemented and works on ios and android

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
 
 /**
  * Generated class for the MapPage page.
@@ -8,18 +8,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+declare let google: any;
 @IonicPage()
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
 })
 export class MapPage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public platform: Platform) {
+    platform.ready().then(() => {
+      this.initMap()
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
+  }
+
+  initMap(){
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 7,
+      center: {lat: 41.85, lng: -87.65}
+    });
   }
 
 }
